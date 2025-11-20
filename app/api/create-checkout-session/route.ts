bc7f8a868302446f4f6e238bf12638f5fc719f42
yt101 <allgoodyt1@gmail.com>
2025-11-19 21:28:04 -0500

    Remove apiVersion from Stripe client initializations

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -4,9 +4,7 @@ import { NextResponse } from "next/server";
 
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-10-29.clover",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(request: Request) {
   try {
